b608f0a6f2a0b283202dd3a23e8ebc4a2aaa56ce
Anup Saud <anupsaud10@gmail.com>
2025-02-03 10:46:52 -0500

    Implement SearchResultVC with table view for displaying city search results

--- BasicWeather/Search/SearchResultVC.swift
+++ BasicWeather/Search/SearchResultVC.swift
@@ -8,13 +8,46 @@
 import UIKit
 
 class SearchResultVC: UIViewController {
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.showsVerticalScrollIndicator = false
+        table.showsHorizontalScrollIndicator = false
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(LocationRow.self, forCellReuseIdentifier: "LocationRow")
+    }
+
+}
+
+extension SearchResultVC : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: LocationRow.id, for: indexPath) as! LocationRow
+        return cell
+    }
+    
+    
+    
+}
+extension SearchResultVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
 }
